0f1db7413769d9c0465657f1d207135ed2c6f6a5
jang1er <jokruger03@gmail.com>
2022-06-11 14:05:30 +0200

    new comment
    Johannes ist dumm. Der braucht das

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -6,6 +6,7 @@ public class Game {
 	
 	public static void main(String[] args) {
 		
+		// make new window
 		JFrame window = new JFrame("Platformer Test");
 		window.setContentPane(new GamePanel());
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
