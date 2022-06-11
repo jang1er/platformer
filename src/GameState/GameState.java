ceb8f7092cd11189de6786bae5feb94b5dac9c20
jang1er <jokruger03@gmail.com>
2022-06-11 13:43:09 +0200

    test

--- src/GameState/GameState.java
+++ src/GameState/GameState.java
@@ -3,9 +3,8 @@ package GameState;
 import java.awt.Graphics2D;
 
 public abstract class GameState {
-	
-	protected GameStateManager gsm;
 
+	protected GameStateManager gsm;
 
 	public abstract void init();
 	public abstract void update();
